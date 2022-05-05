1f8763a1472385bc8926a560c5c386a60129096d
Nishant-Chaudhary5338 <nishantchaudhary5338@gmail.com>
2022-05-05 15:55:54 +0530

    initial commit, setup typescript project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="bg-red-600">HEllo Typescript</div>;
+}
+
+export default App;
